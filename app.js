06fa2569552ff9d4d64c451fea086a530a5307e3
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-07-11 20:52:10 +1000

    added default signup form submit route

--- app.js
+++ app.js
@@ -10,4 +10,51 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/signup.html");
 });
 
+app.post("/", (req,res) =>{
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    
+    const data = {
+        members:[
+            {
+                email_address:email,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME:firstName,
+                    LNAME:lastName
+                }
+            }
+        ]
+    };
+    
+    const jsonData = JSON.stringify(data);
+    const url = "https://"+process.env.MAILCHIMP_DATA_CENTER+".api.mailchimp.com/3.0/lists/"+process.env.MAILCHIMP_LIST_ID;
+    const options = {
+        method: "POST",
+        auth: process.env.MAILCHIMP_USER_NAME+ ":"+process.env.MAILCHIMP_API_KEY,
+    };
+    
+    const request = https.request(url, options, function(response){
+        
+        if (response.statusCode === 200) {
+            
+            res.sendFile(__dirname + "/success.html")
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html")
+            
+        }
+        
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+        
+    });
+    
+    request.write(jsonData);
+    request.end();
+    
+});
+
 app.listen(3000, () => console.log("Server is running on port 3000."))
