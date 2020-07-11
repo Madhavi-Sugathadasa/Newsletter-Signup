15075f0c2cf2abfd299902420290d04182d97074
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-07-11 22:00:59 +1000

    updated app.js

--- app.js
+++ app.js
@@ -64,4 +64,4 @@ app.post("/failure", (req, res) => {
     
 });
 
-app.listen(3000, () => console.log("Server is running on port 3000."))
+app.listen(process.env.PORT || 3000, () => console.log("Server is running on port 3000."))
