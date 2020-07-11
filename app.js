31cd6fff90632f5307918cad6d84f1287d3b2260
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-07-11 20:59:58 +1000

    updated app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser  = require("body-parser");
 const request  = require("request");
+const https = require("https");
 
 const app = express();
 app.use(express.static("public"));
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/signup.html");
